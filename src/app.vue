a87c717ae989703d002977b069d091c1c61294bf
Dslim1996 <ging35@naver.com>
2021-08-07 19:24:33 +0900

    handler

--- src/app.vue
+++ src/app.vue
@@ -1,53 +1,19 @@
 <template>
-    <h1 @click="increase">
-        {{ count }}
-    </h1>
-    <div v-if="count > 4">
-        4보다 큼니다!
-    </div>
-    <ul>
-        <Fruit 
-            v-for="fruit in fruits"
-            :key="fruit"
-            :name="fruit">
-            {{ fruit }}
-        </Fruit>
-        <li 
-            v-for="fruit in fruits"
-            :key="fruit">
-            App.vue {{ fruit }}
-        </li>
-    </ul>
+    <button @click="handlerA(), handlerB()">
+        click me
+    </button>
 </template>
 
 <script>
-import Fruit from '~/components/Fruit'
+
 export default {
-    components: {
-        Fruit
-    },
-    data() {
-        return {
-            count: 0,
-            fruits: ['Apple','Banan','Cherry']
-        }   
-    },
     methods: {
-        increase() {
-            this.count += 1;
+        handlerA(){
+            console.log('A')
+        },
+        handlerB(){
+            console.log('B')
         }
     }
 }
 </script>
-
-<style lang="scss">
-    h1 {
-        font-size: 50px;
-        color:royalblue;
-    }
-    ul {
-        li {
-            font-size: 40px;
-        }
-    }
-</style>
