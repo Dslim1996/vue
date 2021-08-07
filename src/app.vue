254c623fd058c4f3ff86f794a70e8d91dab02f20
Dslim1996 <ging35@naver.com>
2021-08-07 17:40:40 +0900

    computed

--- src/app.vue
+++ src/app.vue
@@ -1,53 +1,26 @@
 <template>
-    <h1 @click="increase">
-        {{ count }}
-    </h1>
-    <div v-if="count > 4">
-        4보다 큼니다!
-    </div>
-    <ul>
-        <Fruit 
-            v-for="fruit in fruits"
-            :key="fruit"
-            :name="fruit">
-            {{ fruit }}
-        </Fruit>
-        <li 
-            v-for="fruit in fruits"
-            :key="fruit">
-            App.vue {{ fruit }}
-        </li>
-    </ul>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
+    <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-import Fruit from '~/components/Fruit'
 export default {
-    components: {
-        Fruit
-    },
     data() {
         return {
-            count: 0,
-            fruits: ['Apple','Banan','Cherry']
-        }   
+            msg: 'Hello Computed!'
+        }
+    },
+    computed:{
+        reversedMessage() {
+            return this.msg.split('').reverse().join('')
+        }    
     },
     methods: {
-        increase() {
-            this.count += 1;
-        }
+        reverseMessage() {
+            return this.msg.split('').reverse().join('')
+        }    
     }
 }
 </script>
-
-<style lang="scss">
-    h1 {
-        font-size: 50px;
-        color:royalblue;
-    }
-    ul {
-        li {
-            font-size: 40px;
-        }
-    }
-</style>
