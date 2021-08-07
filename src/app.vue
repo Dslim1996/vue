872bd9bfe80778f27f475bd7bc96446dbe0c9ca8
Dslim1996 <ging35@naver.com>
2021-08-07 17:31:41 +0900

    computed

--- src/app.vue
+++ src/app.vue
@@ -1,53 +1,13 @@
 <template>
-    <h1 @click="increase">
-        {{ count }}
-    </h1>
-    <div v-if="count > 4">
-        4보다 큼니다!
-    </div>
-    <ul>
-        <Fruit 
-            v-for="fruit in fruits"
-            :key="fruit"
-            :name="fruit">
-            {{ fruit }}
-        </Fruit>
-        <li 
-            v-for="fruit in fruits"
-            :key="fruit">
-            App.vue {{ fruit }}
-        </li>
-    </ul>
+    <Fruits/>
 </template>
 
 <script>
-import Fruit from '~/components/Fruit'
+import Fruits from '~/components/Fruits'
 export default {
     components: {
-        Fruit
-    },
-    data() {
-        return {
-            count: 0,
-            fruits: ['Apple','Banan','Cherry']
-        }   
-    },
-    methods: {
-        increase() {
-            this.count += 1;
-        }
+        Fruits
     }
 }
 </script>
 
-<style lang="scss">
-    h1 {
-        font-size: 50px;
-        color:royalblue;
-    }
-    ul {
-        li {
-            font-size: 40px;
-        }
-    }
-</style>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,50 @@
+<template>
+    <section v-if="hasFruit">
+        <h1>Fruits</h1>
+        <ul>
+            <li 
+            v-for="fruit in fruits" 
+            :key="fruit">
+                {{ fruit }}
+            </li>
+        </ul>
+    </section>
+
+    <section>
+        <h1>Reverse Fruits</h1>
+        <ul>
+            <li 
+            v-for="fruit in reverseFurits" 
+            :key="fruit">
+                {{ fruit }}
+            </li>
+        </ul>
+    </section>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            fruits: [
+                'Apple', 'Banana', 'Cherry'
+            ]
+        }
+    },
+    //계산된 데이터 
+    computed: {
+        hasFruit() {
+            return this.fruits.length > 0
+        },
+        reverseFurits() {
+            return this.fruits.map(fruit => {
+                // 'Apple' => ['A', 'p', 'p', 'l', 'e']
+                // => ['e', 'l', 'p', 'p', 'A'] => 'elppA'
+                return fruit.split('').reverse().join('')
+            })
+        }
+    }
+}
+</script>
+
