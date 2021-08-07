461209aada9f4f57f0defe28662559d4b07f0610
Dslim1996 <ging35@naver.com>
2021-08-07 16:45:09 +0900

    lifecycle

--- src/app.vue
+++ src/app.vue
@@ -1,53 +1,28 @@
 <template>
-    <h1 @click="increase">
-        {{ count }}
-    </h1>
-    <div v-if="count > 4">
-        4보다 큼니다!
-    </div>
-    <ul>
-        <Fruit 
-            v-for="fruit in fruits"
-            :key="fruit"
-            :name="fruit">
-            {{ fruit }}
-        </Fruit>
-        <li 
-            v-for="fruit in fruits"
-            :key="fruit">
-            App.vue {{ fruit }}
-        </li>
-    </ul>
+  <h1>{{  count}}</h1>
 </template>
 
 <script>
-import Fruit from '~/components/Fruit'
 export default {
-    components: {
-        Fruit
-    },
-    data() {
-        return {
-            count: 0,
-            fruits: ['Apple','Banan','Cherry']
-        }   
-    },
-    methods: {
-        increase() {
-            this.count += 1;
-        }
+  data() {
+    return {
+      count: 2
     }
+  },
+  beforeCreate() {
+    console.log('beforCreate!', this.count)
+  },
+  created() {
+    console.log('Created!', this.count);
+    console.log(document.querySelector('h1'));
+  },
+  beforeMount() {
+    console.log('Befor Mount!')
+    console.log(document.querySelector('h1'));
+  },
+  mounted() {
+    console.log('mounted!');
+    console.log(document.querySelector('h1'));
+  }
 }
 </script>
-
-<style lang="scss">
-    h1 {
-        font-size: 50px;
-        color:royalblue;
-    }
-    ul {
-        li {
-            font-size: 40px;
-        }
-    }
-</style>
