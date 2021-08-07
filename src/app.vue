74a4398696fe1f21a2616a1b540a0c912658f242
Dslim1996 <ging35@naver.com>
2021-08-07 19:50:33 +0900

    event

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,31 @@
 <template>
-    <button @click="handlerA(), handlerB()">
-        click me
-    </button>
+    <a 
+    href="https://naver.com"
+    target="_blank"
+    @click.prevent.once="handler">
+    NAVER
+    </a>
+
+    <div 
+    class="parent"
+    @wheel.passive="handler">
+        <div 
+        class="child"></div>
+    </div>
 </template>
 
 <script>
 
 export default {
     methods: {
-        handlerA(){
+        handler(event){
+            for(let i = 0; i < 10000; i++){
+                console.log(event)
+            }
+        },
+        handlerA(event){
+            console.log(event.target)
+            console.log(event.currentTarget)
             console.log('A')
         },
         handlerB(){
@@ -17,3 +34,19 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    .parent{
+        width: 200px;
+        height: 100px;
+        background-color: royalblue;
+        margin: 10px;
+        padding: 10px;
+        overflow: auto;
+        .child{
+            width: 100px;
+            height: 2000px;
+            background-color: orange;
+        }
+    }
+</style>
